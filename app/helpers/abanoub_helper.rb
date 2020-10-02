d49f5e74770d3c67d8179319c09d79c486cc641f
CBU ACM <acm@ubuntu.attlocal.net>
2020-10-02 02:51:59 +0000

    Hi Nick

---
+++ app/helpers/abanoub_helper.rb
@@ -0,0 +1,2 @@
+module AbanoubHelper
+end
---
+++ test/controllers/abanoub_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AbanoubControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
